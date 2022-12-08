0a78718da24a96d35e1e01f2a22b3b6f700914c9
tedygreat <tedygreat@gmail.com>
2022-12-08 12:06:29 -0800

    tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+	 */
+
 
 
 int main(void)
-
 {
 
 		printf("Size of a char: %zu byte(s)\n", sizeof(char));
