dd4bb7e1297479a7396c6de31a0a26ec3f353f5d
tedygreat <tedygreat@gmail.com>
2022-12-08 11:21:38 -0800

    hj

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,28 +1,6 @@
-/*
- *
- *  * File: 6-size.c
- *
- *   * Auth: Brennan D Baraban
- *
- *    */
-
-
-
 #include <stdio.h>
 
 
-
-/**
- *
- *  * main - Prints the size of various types based on
- *
- *   *        the computer it is compiled and run on..
- *
- *    *
- *
- *     * Return: Always 0.
- *
- *      */
 
 int main(void)
 
