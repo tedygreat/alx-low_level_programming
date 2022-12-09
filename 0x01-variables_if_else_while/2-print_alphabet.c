5c886f9082c2af297aee93174d0d662a8c2cde8e
tedygreat <tedygreat@gmail.com>
2022-12-09 12:30:08 -0800

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - alphabet
+ * Description: 'use putcher'
+ * Return: Always 0.
+	*/
+int main(void)
+{
+	char l;
+for (l = 'a'; l <= 'z'; l++)
+	putchar(l);
+putchar('\n');
+	return (0);
+}
