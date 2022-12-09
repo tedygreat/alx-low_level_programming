d812513be02e8fbc4c16f959b9e7fc81df39d393
tedygreat <tedygreat@gmail.com>
2022-12-09 13:37:23 -0800

    f

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - lower case
+ * Description: 'task 7'
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char l;
+for (l = 'z'; l >= 'a'; l--)
+	putchar(l);
+putchar('\n');
+	return (0);
+}
