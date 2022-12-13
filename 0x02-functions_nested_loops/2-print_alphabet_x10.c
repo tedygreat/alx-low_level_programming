b127eff1a6b166c14274701d6b3e0483e031bc5f
tedygreat <117822622+tedygreat@users.noreply.github.com>
2022-12-13 13:07:16 -0800

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,16 @@
-i
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_alphabet - print the alphabet of lower ca
+ */
+void print_alphabet(void)
+{
+		char l;
+  int count;
+  for (coun = 0; count < 10; count++)
+  {
+	for (l = 'a'; l <= 'z'; l++)
+		_putchar(l);
+	_putchar('\n');
+  }
+}
