2acc26a25e56c7f7169dab5f9c83debea54d1277
tedygreat <tedygreat@gmail.com>
2022-12-13 12:29:40 -0800

    task1

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_alphabet - print the alphabet of lower ca
+ */
+void print_alphabet(void)
+{
+		char l;
+	for (l = 'a'; l <= 'z'; l++)
+		_putchar(l);
+	_putchar('\n');
+}
