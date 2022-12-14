6b99136250d2c10f4d9810a1208c742a844be2d3
tedygreat <tedygreat@gmail.com>
2022-12-13 18:47:08 -0800

    ss

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,33 +8,33 @@
 void print_times_table(int n)
 {
 		int nu, m, p;
-	if (n >= 0 && n <= 15)
+if (n >= 0 && n <= 15)
+{
+	for (nu = 0; nu <= n; nu++)
 	{
-		for (nu = 0; nu <= n; nu++)
+		_putchar('0');
+		for (m = 1; m <= n; m++)
 		{
-	_putchar('0');
-for (m = 1; m <= n; m++)
-	{
-	_putchar(',');
-	_putchar(' ');
-	p = nu * m;
-		if (p <= 99)
+			_putchar(',');
 			_putchar(' ');
-		if (p <= 9)
+			p = nu * m;
+			if (p <= 99)
 			_putchar(' ');
-		if (p >= 100)
-		{
+			if (p <= 9)
+			_putchar(' ');
+			if (p >= 100)
+			{
 			_putchar((p / 100) + '0');
 			_putchar(((p / 10) % 10) + '0');
+			}
+			else if (p <= 99 && p >= 10)
+			{
+				_putchar((p / 10) + '0');
+			}
+			_putchar((p % 10) + '0');
 		}
-		else if (p <= 99 && p >= 10)
-		{
-			_putchar((p / 10) + '0');
-		}
-		_putchar((p % 10) + '0');
+		_putchar('\n');
 	}
-	_putchar('\n');
-		}
-	}
+}
 
 }
