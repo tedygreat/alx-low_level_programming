da0877d6bc0dae1ab1150ceab805b554ab3930e6
tedygreat <tedygreat@gmail.com>
2022-12-15 13:44:33 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - function that perform multiplication
+ * @a: first variable
+ * @b: second variable
+ * Return: lways return result;
+ */
+
+int mul(int a, int b)
+{
+		int result;
+			result = a * b;
+	return (result);
+}
