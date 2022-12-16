7b8d0aee7eef1770873455acf1c8301e72d5e3a7
tedygreat <tedygreat@gmail.com>
2022-12-15 18:45:58 -0800

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - main fun
+ * Description: print number
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+		int i;
+		int mult;
+		int mult1;
+		int mult2;
+	for (i = 1; i <= 100; i++)
+	{
+		mult = i % 3;
+		mult1 = i % 5;
+		mult2 = i % 15;
+		if (mult == 0)
+		{
+			printf("Fizz");
+		}
+		else if (mult1 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (mult2 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+			printf("%d", i);
+		if (i !=0)
+			putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
