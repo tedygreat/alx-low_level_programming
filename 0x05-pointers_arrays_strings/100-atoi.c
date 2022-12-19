f21ac3479fcacc50c3c6b80f160d9a42289019f2
tedygreat <tedygreat@gmail.com>
2022-12-19 14:43:20 -0800

    task m

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _atoi - func
+ * @s: input
+ * Return:Always 0.
+ */
+
+int _atoi(char *s)
+{
+	int sign;
+	unsigned int  num;
+	char *var;
+			var = s;
+			num = 0;
+			sign = 1;
+
+	while (*var != '\0' && (*var < '0' || *var > '9'))
+	{
+		if (*var == '-')
+
+			sign = sign * -1;
+		var++;
+	}
+
+	if (*var != '\0')
+	{
+		do{
+			num = num * 10 +(*var - '0');
+			var++;
+		}
+		while(*var >= '0' && *var <= '9');
+	}
+
+
+	return (num *sign);
+}
