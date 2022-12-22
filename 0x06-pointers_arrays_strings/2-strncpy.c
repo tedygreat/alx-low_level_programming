1f4a25cba3ac2bb5e1ffc551023b75cbcc558bb3
tedygreat <tedygreat@gmail.com>
2022-12-22 09:51:22 -0800

    k

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,13 +10,15 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-		int index;
-		int j;
+		int index = 0;
+		int j = 0;
+	while (src[index++])
+		j++;
 	for (index = 0; src[index] != 0 && index < n; index++)
 	{
 		dest[index] = src[index];
 	}
-	for (j = index; j < n; j++)
+	for (index = j; index < n; index++)
 	{
 		dest[index] = 0;
 	}
