09822d7605f689134e27ad2b79e12e4cf45d369f
tedygreat <tedygreat@gmail.com>
2022-12-22 11:53:54 -0800

    task10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
-/**
- * main - fun
- * Return: Always 0.
- */
+
 int main(void)
 {
 	int n;
@@ -23,5 +20,4 @@ int main(void)
 /* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
-
 }
