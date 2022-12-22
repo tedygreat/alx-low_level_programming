a06cae7ad6fa78bab8730aaeeea957ec28eadb14
tedygreat <tedygreat@gmail.com>
2022-12-22 10:25:24 -0800

    k

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - function that incode root13
+ * @s: var
+ * Return: encod char
+ */
+
+char *rot13(char *s)
+{
+		char alph[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+		char encode[] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+		int i = 0, j;
+		char c;
+	while (s[i] != 0)
+	{
+		c = s[i];
+		for (j = 0; j < 52; j++)
+		{
+			if (c == alph[j])
+			{
+				s[i] = encode[j];
+			}
+		}
+		i++;
+	}
+	
+	return (s);
+}
