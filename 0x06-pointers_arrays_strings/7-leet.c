766c7b28dfbf24a4418e9065a47256486a5f9edd
tedygreat <tedygreat@gmail.com>
2022-12-22 09:43:11 -0800

    task7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,22 +2,22 @@
 
 /**
  * leet - function tha encodes string to 1337
+ * @s: var
  * Return: char
  */
 
 char *leet(char *s)
 {
-	char low_up[5][2] = {{'a', 'A'}, {'e', 'E'}, {'o', 'O'},
-		{'t', 'T'}, {'l', 'L'}};
+		char l[5][2] = {{'a', 'A'}, {'e', 'E'}, {'o', 'O'}, {'t', 'T'}, {'l', 'L'}};
 		char values[] = {'4', '3', '0', '7', '1'};
 		int i, j;
+		char c;
 	for (i = 0; s[i] != 0; i++)
 	{
-
-			char c = s[i];
+		c = s[i];
 		for (j = 0; j < 5; j++)
 		{
-			if (c == low_up[j][0] || c == low_up[j][1])
+			if (c == l[j][0] || c == l[j][1])
 			{
 				s[i] = values[j];
 				break;
