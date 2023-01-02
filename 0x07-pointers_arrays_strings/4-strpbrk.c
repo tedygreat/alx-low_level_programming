76142277df7489af899bcaa235f20bc9d6c0b34c
tedygreat <tedygreat@gmail.com>
2023-01-02 10:30:17 -0800

    t4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function that perform search astring
+ * @s: var
+ * @accept: var
+ * Return: pointer byte
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+	int k = 0;
+	int m = 0;
+	while (s[k])
+		k++;
+	while (accept[m])
+		m++;
+	for (i = 0; i < s; i++)
+		for (j =0; j <accept; accept++)
+			if (s[i] == accept[j])
+				return (s);
+
+	return ('\0');
+}
