9a47651a8e7db5545cd78e7732b307697112dd42
tedygreat <tedygreat@gmail.com>
2023-01-02 06:44:58 -0800

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy - function that perform copy memory erea
+ * @dest: var
+ * @src: var1
+ * @n: var2
+ * Return: pointer dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+		unsigned int i;
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
