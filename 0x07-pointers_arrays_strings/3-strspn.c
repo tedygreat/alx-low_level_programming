4e68b28e16d9448053fe544d86b14dd34b36b943
tedygreat <tedygreat@gmail.com>
2023-01-02 09:46:46 -0800

    task4

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,20 +11,21 @@ unsigned int _strspn(char *s, char *accept)
 {
 		int i = 0;
 		int j;
+		int index;
 		
 			int count = 0;
 	while (s[i])
 		i++;
 	for (j = 0; j < i; j++)
 	{
-		if (*s == accept[i])
-		{
-			count++;
-			break;
-		}
-		
-		else if (accept[i + 1] == '\0')
-			return (count);
+		for (index = 0; accept[index]; index++)
+			if (*s == accept[i])
+			{
+				count++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+				return (count);
 		
 	}
 	return (count);
