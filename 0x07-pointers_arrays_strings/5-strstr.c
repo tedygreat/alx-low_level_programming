996995a660752c7af401725a5cfc82c0b5e1a855
tedygreat <tedygreat@gmail.com>
2023-01-02 11:40:48 -0800

    task5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strstr - function that locates a substring.
+ * @haystack: var
+ * @needle: var1
+ * Return: pointer to change the beginning
+ */
+char *_strstr(char *haystack, char *needle)
+{
+		int index = 0;
+	while (*haystack)
+	{
+		if (haystack[index] == needle[index])
+		{
+			do {
+				if (needle[index + 1] == '\0')
+					return (haystack);
+				index++;
+			} while (haystack[index] == needle[index]);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
