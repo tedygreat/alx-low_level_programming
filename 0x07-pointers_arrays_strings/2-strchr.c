26c30311d14193956dcc0762f9d97a9f8cefe495
tedygreat <tedygreat@gmail.com>
2023-01-02 07:21:15 -0800

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - function that perform locate character in astring
+ * @s: var
+ * @c: char var
+ * Return: pointer c
+ */
+
+char *_strchr(char *s, char c)
+{
+		int i = 0;
+		int b;
+	while (s[i])
+		i++;
+	for (b = 0; b < i; b++)
+	{
+		s += b;
+		return (s);
+	}
+
+	return ('\');
+}
