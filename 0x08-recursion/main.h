7878e8764fc94df4e69e4ecb17a4a433a7ddebe8
tedygreat <tedygreat@gmail.com>
2023-01-04 06:08:35 -0800

    task0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,9 +1,9 @@
-#ifndef MAIN.H
-#define MAIN.H
-
-#include <stddef>
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
 
 int _putchar(char c);
+void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
