800b90e0df93108c0061235bf9a034269a652b27
tedygreat <tedygreat@gmail.com>
2023-01-04 14:22:14 -0800

    task adv2

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * wildcmp - function that perform comper two string 
+ * wildcmp - function that perform comper two string
  * @s1: var
  * @s2: var1
- * Return: 1 if true 
+ * Return: 1 if true
  */
 
 int wildcmp(char *s1, char *s2)
