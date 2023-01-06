a8c1c03a3776b60e1946fc8396693bb45b5b2c1d
tedygreat <tedygreat@gmail.com>
2023-01-06 13:16:27 -0800

    t1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - fun
+ * @argc: var
+ * @argv: arg
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%i\n", argc - 1);
+	return (0);
+}
