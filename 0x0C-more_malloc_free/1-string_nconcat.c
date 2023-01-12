651bb86a467098a09817bf60a6d1c590255466c4
tedygreat <tedygreat@gmail.com>
2023-01-12 05:36:56 -0800

    task 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = sz2;
 	con = malloc((sz1 + n) * sizeof(char));
 	if (con == NULL)
-		return (0);
+		return (NULL);
 	for (i = 0; i < sz1; i++)
 	{
 		con[i] = s1[i];
