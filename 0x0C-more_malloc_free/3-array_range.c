1376be2691767c7042e28623b24c1b6d44476bc7
tedygreat <tedygreat@gmail.com>
2023-01-12 09:32:49 -0800

    task 4

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,18 +11,19 @@
 
 int *array_range(int min, int max)
 {
-		int i;
-		char *con;
+		int i, lenth;
+		int *con;
+			lenth = (max - min) + 1;
 	if (min > max)
 	{
 		return (0);
 	}
-			con = malloc(4 * (max - min) + 1);
+			con = malloc(sizeof(int) * lenth);
 	if (con == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < ((min - max) + 1); i++)
+	for (i = 0; i < lenth; i++)
 	{
 		con[i] = min++;
 	}
