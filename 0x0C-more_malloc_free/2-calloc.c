fc9005212cffd1afe9b3094fccbc6deeffb70399
tedygreat <tedygreat@gmail.com>
2023-01-12 07:13:11 -0800

    task2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _calloc -function that perform allocate memory of an array
+ * @nmemb: var
+ * @size: var2
+ * Return: nothing
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+		void *m;
+		char *fill;
+		unsigned int i;
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	m = malloc(nmemb * size);
+	if (m == NULL)
+		return (NULL);
+		fill = m;
+	for (i = 0; i < (size * nmemb); i++)
+		fill[i] = '\0';
+	return (m);
+}
