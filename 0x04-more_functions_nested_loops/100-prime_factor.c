293050a46805e6411f1fcc13a563410cc4164efb
tedygreat <tedygreat@gmail.com>
2023-01-13 14:04:07 -0800

    pf

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,6 +9,20 @@
 int main(void)
 
 {
+		int i, n;
+		n = 612852475143;
+		i =2
+	while (i < n)
+	{
+		if (n % i == 0)
+		{
+			n = n / 2;
+			printf ("%d", i);
+		}
+		i++;
+	}
+
+
 
 	return (0);
 }
