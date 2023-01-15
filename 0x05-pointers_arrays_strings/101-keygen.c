94bcefdadfc87b11b0f6aee0007922c3b5707339
tedygreat <tedygreat@gmail.com>
2023-01-15 09:23:33 -0800

    t

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
 /**
- *main - func
- * @s: input
+ * main - func
  * Return:Always 0.
  */
 
 int main(void)
 {
-	int pass;
+		int pass;
+			int sum = 0;
+	while (sum < 3000)
+	{
 	pass = (rand() % 128);
+	sum = sum + pass;
 	printf("%c", pass);
+	}
+	printf("%c", 6000 - pass);
 	return (0);
 }
