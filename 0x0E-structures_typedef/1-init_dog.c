41b76a1bca7ef8c69113593827a4342efe9e3147
tedygreat <tedygreat@gmail.com>
2023-01-16 07:18:19 -0800

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog - function that initialize variable
+ * @d: var
+ * @name: var1
+ * @age: var3
+ * @owner: var3
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d.name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
+
