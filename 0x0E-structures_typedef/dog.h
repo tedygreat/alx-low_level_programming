93d7837edef7286f496b6f6245766824ab0e9293
tedygreat <tedygreat@gmail.com>
2023-01-16 06:32:49 -0800

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct my_struct - func
+ * @name: var
+ * @age: var1
+ * @owner: var2
+ * Description: structurer data type
+ */
+
+struct my_struct
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
