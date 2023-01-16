f0ab7725ca06264d00ea586e5e5cf2e2cbe58901
tedygreat <tedygreat@gmail.com>
2023-01-16 07:30:02 -0800

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+#include <stddef.h>
 /**
  * init_dog - function that initialize variable
  * @d: var
@@ -12,7 +12,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		d.name = name;
+		d->name = name;
 		d->age = age;
 		d->owner = owner;
 	}
