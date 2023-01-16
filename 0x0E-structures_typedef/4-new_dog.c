dd0f9864f443c82db49eadf60c2d4222012a045b
tedygreat <tedygreat@gmail.com>
2023-01-16 12:40:37 -0800

    task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,1 +1,22 @@
 #include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - function that create new dog
+ * @name: var
+ * @age: var1
+ * @owner: var2
+ * Return: new structur of dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+		dog_t *dog_new;
+	if (name == NULL || age < 0 || NULL || owner == NULL)
+		return (NULL);
+	dog_new = malloc(sizeof(dog_t));
+	if (dog_new == NULL)
+		return (NULL);
+	return (dog_new);
+}
