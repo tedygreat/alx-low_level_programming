66a815f40c572864a73fcd5450151b4414034cb3
tedygreat <tedygreat@gmail.com>
2023-01-16 13:03:59 -0800

    t

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int str_len(char *str);
+char *_str_cpy(char *des, char *src);
 /**
  * new_dog - function that create new dog
  * @name: var
@@ -19,4 +21,38 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog_new == NULL)
 		return (NULL);
 	return (dog_new);
+}
+
+/**
+ * str_len - function that perform string lenth
+ * @str: string variable
+ * Return: length of string
+ */
+
+int str_len(char *str)
+{
+		int lenth;
+			lenth = 0;
+	while (str[lenth])
+		lenth++;
+	return (lenth);
+}
+
+
+/**
+ * _str_cpy -function copy string
+ * @des: var
+ * @src: var1
+ * Return: copy string
+ */
+
+char *_str_cpy(char *des, char *src)
+{
+		int i;
+	for (i = 0; src[i]; i++)
+	{
+		src[i] = des[i];
+	}
+	des[i] = '\0';
+	return (des);
 }
