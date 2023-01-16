82461c5fe584e127f591396ccb5710b5cc2a2432
tedygreat <tedygreat@gmail.com>
2023-01-16 07:48:04 -0800

    t2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,40 @@
+#include "dog.h"
+#include <stddef>
+
+/**
+ * print_dog - function that print dog
+ * @d: var
+ * Return: nothing
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL)
+	{
+		printf("name: (nil)\n");
+	}
+	else
+	{
+		printf("name: %s\n", d->name);
+	}
+	if (d->age == NULL)
+	{
+		printf("age: (nil)\n");
+	}
+	else
+	{
+		printf("age: %s\n", d->age);
+	}
+	if (d->owner == NULL)
+	{
+		printf("owner: (nil)\n");
+	}
+	else
+	{
+		printf("owner: %s\n", d->owner);
+	}
+}
