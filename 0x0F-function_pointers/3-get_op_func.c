a78c1b28636fc4fc6ffa9381de42e1690a229a25
tedygreat <tedygreat@gmail.com>
2023-01-18 10:08:32 -0800

    taa3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
-
 /**
  * get_op_func - function pointer to select correct func
  * @s: operater var
@@ -8,8 +9,6 @@
  
 int (*get_op_func(char *s))(int, int)
 {
-		int i;
-			i = 0;
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
@@ -18,6 +17,9 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
+	
+	int i = 0;
+
 	while (ops[i].op)
 	{
 		if (strcmp(ops[i].op, s) == 0)
